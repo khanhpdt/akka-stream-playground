44f01e6203c6e5013ee69f037d29dc208f7e14b4
Khanh Pham <khanhpdtvn@gmail.com>
2020-07-27 10:03:52 +0700

    Another testing

---
+++ src/main/scala/vn/khanhpdt/akkastream/playground/FastProducerSlowConsumerWithThrottle3.scala
@@ -0,0 +1,34 @@
+package vn.khanhpdt.akkastream.playground
+
+import scala.concurrent.duration._
+
+import akka.actor.ActorSystem
+import akka.stream._
+import akka.stream.scaladsl._
+import com.typesafe.scalalogging.StrictLogging
+
+object FastProducerSlowConsumerWithThrottle3 extends App with StrictLogging {
+
+  implicit val system: ActorSystem = ActorSystem("QuickStart")
+
+  // Producing 5 per 5 seconds. But b/c the processor is slow (1 per 2 seconds),
+  // the whole flow is slowing down to 1 per 2 seconds.
+  val fastProducerSlowConsumerGraph =
+    Source.repeat(true)
+      .throttle(5, 5.second, 1, ThrottleMode.Shaping)
+      .via {
+        Flow.fromFunction(s => {
+          logger.info(s"Flow received $s. Sleep for 2 seconds.")
+          Thread.sleep(2.seconds.toMillis)
+          s"From Flow: $s"
+        })
+      }
+      .to {
+        Sink.foreach(s => {
+          logger.info(s"Sink received $s")
+        })
+      }
+
+  fastProducerSlowConsumerGraph.run()
+
+}
